020e268af590445b55291b9549b05fc2659482aa
JoneCoder <mohummadshorifulislam@gmail.com>
2021-02-02 00:36:24 +0600

    done

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Vue.js App"></HelloWorld>
+  <div>
+    <HelloWorld msg="Welcome to Vue.js App"></HelloWorld>
+    <div v-if="isShowing">
+      <div v-show="isShow" v-html="title"></div>
+      <h3 v-show="isShow === null">Vuejs Tutorial</h3>
+      <h4 v-show="isShow" v-once>{{ count }}</h4>
+    </div>
+    <div v-else-if="isShowing === null" v-text="mobile"></div>
+    <div v-else v-text="name"></div>
+    <div>
+      <ul>
+        <li v-for="(item, index) in items" :key="index">{{ index }} : {{ item.color }}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +23,26 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      title: '<h1>Hello Vuejs</h1>',
+      isShowing: true,
+      isShow: true,
+      count: 0,
+      name: 'Md. Shariful Islam',
+      mobile: '01712074151',
+      items: [
+        {color : 'red'},
+        {color : 'green'},
+        {color : 'blue'},
+      ],
+    }
+  },
+  mounted(){
+    setInterval(() => {
+      this.count++;
+    }, 1000);
   }
 }
 </script>
